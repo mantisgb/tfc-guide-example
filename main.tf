e04e7258862f710feb9855adab5b519cb1400a4e
Keith Richards <41892584+mantisgb@users.noreply.github.com>
2022-03-25 17:24:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,43 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "management" {
+  name        = "Expedition Management"
+  description = "Allow management of Expedition"
+  vpc_id      = aws_vpc.inbound.id
+
+  # Egress All
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Ingress TCP/22
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [local.my_ip]
+  }
+
+  # Ingress TCP/443
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [local.my_ip]
+  }
+
+  # Ingress ICMP
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = [local.my_ip]
+  }
+}
+
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
